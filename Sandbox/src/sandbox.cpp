de8663f411736a9fb55adaf845b6e438f808fd7e
Ewan McAndrew <ewan.m@hotmail.com>
2019-09-02 18:07:34 +0100

    Shader loading from file.

--- Sandbox/src/sandbox.cpp
+++ Sandbox/src/sandbox.cpp
@@ -125,41 +125,8 @@ public:
         )";
 
         oFlatShader.reset(BlueMarble::Shader::Create(flatVertexShaderSrc, flatFragmentShaderSrc));
-
-        std::string texVertexShaderSrc = R"(
-            #version 330 core
-
-            layout(location = 0) in vec3 aPosition;
-            layout(location = 1) in vec2 aTexCoord;
-
-            uniform mat4 uViewProjection;
-            uniform mat4 uTransform;
-
-            out vec2 vTexCoord;
-
-            void main()
-            {
-                vTexCoord = aTexCoord;
-                gl_Position = uViewProjection * uTransform * vec4(aPosition, 1.0);
-            }
-        )";
-
-        std::string texFragmentShaderSrc = R"(
-            #version 330 core
-
-            layout(location = 0) out vec4 color;
-
-            in vec2 vTexCoord;
-
-            uniform sampler2D uTexture;
-
-            void main()
-            {
-                color = texture(uTexture, vTexCoord);
-            }
-        )";
-
-        oTextureShader.reset(BlueMarble::Shader::Create(texVertexShaderSrc, texFragmentShaderSrc));
+        
+        oTextureShader.reset(BlueMarble::Shader::Create("assets/shaders/texture.glsl"));
 
         oTexture = BlueMarble::Texture2D::Create("assets/textures/grass.png");
         oTexture2 = BlueMarble::Texture2D::Create("assets/textures/test.png");
